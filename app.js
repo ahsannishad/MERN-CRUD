41d57058735dd489afa31e595da0f0e2e733afaa
Ahsan Nishad <ahsannishad@gmail.com>
2020-11-25 03:22:43 +0600

    production setup modified

--- app.js
+++ app.js
@@ -35,11 +35,6 @@ const postsSchema = new mongoose.Schema({
 
 const Posts = new mongoose.model("Posts", postsSchema);
 
-// Production
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
-}
-
 // Routes
 app.get("/posts", (req, res) => {
 	Posts.find({}, (error, post) => {
@@ -51,9 +46,14 @@ app.get("/posts", (req, res) => {
 	});
 });
 
-app.get("/*", (req, res) => {
-	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+// Production
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
+	app.get("/*", (req, res) => {
+		res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+	});
+}
+
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
 	console.log(`Server started at port ${port}`);
