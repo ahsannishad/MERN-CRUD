4ec6add3849126e030613d08aeacc3704966264d
Ahsan Nishad <ahsannishad@gmail.com>
2020-11-24 22:22:32 +0600

    sending index.html

--- app.js
+++ app.js
@@ -13,11 +13,15 @@ app.use("/posts", require("./routes/route"));
 // Production
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
-
-	app.get("*", function (req, res) {
-		res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-	});
 }
+
+app.get("/*", function (req, res) {
+	res.sendFile(path.join(__dirname, "client/build/index.html"), function (err) {
+		if (err) {
+			res.status(500).send(err);
+		}
+	});
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
