691d8e8a3441601fd1ed151e8efb71270adb4406
Ahsan Nishad <ahsannishad@gmail.com>
2020-11-24 22:48:00 +0600

    poduction ready

--- app.js
+++ app.js
@@ -7,13 +7,13 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-// Routes
-app.use("/posts", require("./routes/route"));
-
 // Production
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
 }
+
+// Routes
+app.use("/posts", require("./routes/route"));
 
 app.get("/*", function (req, res) {
 	res.sendFile(
