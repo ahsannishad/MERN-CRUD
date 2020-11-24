c4698f0899f5aaaf8e30a598a1fa9ef0b1f865d0
Ahsan Nishad <ahsannishad@gmail.com>
2020-11-24 23:45:58 +0600

    deploy

--- app.js
+++ app.js
@@ -38,10 +38,6 @@ const Posts = new mongoose.model("Posts", postsSchema);
 // Production
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static("client/build"));
-
-	app.get("*", (req, res) => {
-		res.sendFile(path.join(__dirname, "client", "build", "index.html")); // relative path
-	});
 }
 
 // Routes
@@ -55,6 +51,9 @@ app.get("/posts", (req, res) => {
 	});
 });
 
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "build", "index.html")); // relative path
+});
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
 	console.log(`Server started at port ${port}`);
