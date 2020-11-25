a1511ae9c274c982292d0c60bc618b40f1123377
Ahsan Nishad <ahsannishad@gmail.com>
2020-11-25 15:24:31 +0600

    express static folder specifying

--- app.js
+++ app.js
@@ -1,58 +1,19 @@
 const express = require("express");
 require("dotenv").config();
 const path = require("path");
-const mongoose = require("mongoose");
 
 const app = express();
-
+app.use(express.static("client/build"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-//mongoose connect
-
-mongoose
-	.connect(process.env.MONGODB_URI, {
-		useNewUrlParser: true,
-		useUnifiedTopology: true,
-		useCreateIndex: true,
-		useFindAndModify: false,
-	})
-	.then(() => {
-		console.log("Database connected");
-	})
-	.catch((err) => {
-		console.log(`Database error ${err.message}`);
-	});
-
-const postsSchema = new mongoose.Schema({
-	title: {
-		type: String,
-	},
-	description: {
-		type: String,
-	},
-});
-
-const Posts = new mongoose.model("Posts", postsSchema);
-
 // Routes
-app.get("/posts", (req, res) => {
-	Posts.find({}, (error, post) => {
-		if (error) {
-			res.json(error.message);
-		} else {
-			res.json(post);
-		}
-	});
-});
+app.use("/api/posts", require("./routes/route"));
 
 // Production
-if (process.env.NODE_ENV === "production") {
-	app.use(express.static(path.join(__dirname, "client", "build", "static")));
-	app.get("/*", (req, res) => {
-		res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-	});
-}
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
