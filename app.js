cb8b1a5f829e3d585a3656b89466f35882b5dcf5
Ahsan Nishad <ahsannishad@gmail.com>
2020-11-25 14:57:59 +0600

    static folder specified

--- app.js
+++ app.js
@@ -48,7 +48,7 @@ app.get("/posts", (req, res) => {
 
 // Production
 if (process.env.NODE_ENV === "production") {
-	app.use(express.static("client/build"));
+	app.use(express.static(path.join(__dirname, "client", "build")));
 	app.get("/*", (req, res) => {
 		res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 	});
