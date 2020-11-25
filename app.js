37c84b2f072cb0f937c25c7357e4bb492ce73dbd
Ahsan Nishad <ahsannishad@gmail.com>
2020-11-25 15:32:25 +0600

    static folder specifying at top

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 const path = require("path");
 
 const app = express();
-app.use(express.static("client/build"));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+app.use(express.static("client/build"));
 
 // Routes
 app.use("/api/posts", require("./routes/route"));
