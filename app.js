11cfc4ca99d0aeb8d77f244837b3e6d4f76b03b7
Ahsan Nishad <ahsannishad@gmail.com>
2020-11-25 15:48:20 +0600

    node env setup to productionF

--- app.js
+++ app.js
@@ -10,11 +10,14 @@ app.use(express.json());
 app.use("/api/posts", require("./routes/route"));
 
 // Production
-app.use(express.static("client/build"));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static("client/build"));
 
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});
+	app.get("*", (req, res) => {
+		res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+	});
+}
+console.log(process.env.NODE_ENV);
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
